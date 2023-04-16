01363b3920beb4c8c074e0836d699181218f3282
IronIdk <jmsfo@iscte-iul.pt>
2023-04-16 21:57:08 +0100

    Continued editing the CloudTo_MQTT folder

--- CloudTo_MQTT/writeToMQTT.java
+++ CloudTo_MQTT/writeToMQTT.java
@@ -1,17 +1,16 @@
-package CloudTo_ReplicaSet;
+package CloudTo_MQTT;
 
-import org.eclipse.paho.client.mqttv3.MqttException;
-import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.*;
 
 public class writeToMQTT {
-    private static String cloud_topic = "topic_name";
-    private static MqttClient mqttclient = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
+    private static final MqttClient mqttclient = new MqttClient("tcp://localhost:1883", MqttClient.generateClientId());
 
     public static void publishSensor(String leituraSensor) {
         try {
             MqttMessage mqtt_message = new MqttMessage();
             mqtt_message.setPayload(leituraSensor.getBytes());
             mqttclient.connect();
+            String cloud_topic = "topic_name";
             mqttclient.publish(cloud_topic, mqtt_message);
             mqttclient.disconnect();
         } catch (MqttException e) {
