4e8000003df963d7e4c5c224ba0d7b2df111a8f7
IronIdk <jmsfo@iscte-iul.pt>
2023-04-18 18:54:18 +0100

    Updated writeToMQTT

--- CloudTo_MQTT/CloudTo_MQTT/writeToMQTT.java
+++ CloudTo_MQTT/CloudTo_MQTT/writeToMQTT.java
@@ -1,17 +1,20 @@
 package CloudTo_MQTT;
 
-import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+
+import javax.swing.*;
+import java.awt.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Random;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.*;
 
 public class writeToMQTT {
     private static final MqttClient mqttclient;
     private static final String BROKER_URL = "tcp://localhost:1883";
-    private static boolean sendingData = true;
+    private volatile static boolean sendingData = true;
+    private static final Object lock = new Object();
 
     static {
         try {
@@ -27,9 +30,14 @@ public class writeToMQTT {
             mqtt_message.setPayload(message.getBytes());
             mqttclient.connect();
             mqttclient.publish(topic, mqtt_message);
-            mqttclient.disconnect();
         } catch (MqttException e) {
             e.printStackTrace();
+        } finally {
+            try {
+                mqttclient.disconnect();
+            } catch (MqttException e) {
+                e.printStackTrace();
+            }
         }
     }
 
@@ -41,52 +49,64 @@ public class writeToMQTT {
         int to = 3;
         double temperature = 9;
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSSSSS");
-        Random rand = new Random();
+        Random rand = new Random(123456789);
 
         // Create the JFrame and buttons
         JFrame frame = new JFrame("Write to MQTT");
+        JTextArea textArea = new JTextArea(20, 100);
+        textArea.setFont(new Font("Monospaced", Font.PLAIN, 14));
+        frame.getContentPane().add(new JScrollPane(textArea), BorderLayout.CENTER);
+
         JPanel buttonPanel = new JPanel(new FlowLayout());
         JButton stopButton = new JButton("Stop Sending Data");
         JButton startButton = new JButton("Start Sending Data");
         buttonPanel.add(stopButton);
         buttonPanel.add(startButton);
         frame.getContentPane().add(buttonPanel, BorderLayout.SOUTH);
+
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+
 
         // Add action listeners to the buttons
-        stopButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendingData = false;
-            }
-        });
+        stopButton.addActionListener(e -> sendingData = false);
 
-        startButton.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                sendingData = true;
-            }
+        startButton.addActionListener(e -> {
+            sendingData = true;
+            resumeSendingData();
         });
 
         // Start sending data
-        while (true) {
-            if (sendingData) {
-                LocalDateTime now = LocalDateTime.now();
-                if (rand.nextDouble() < 0.2) { // randomly end an experience with 20% probability
-                    String end_msg = "{hour:" + formatter.format(now) + ", from:0, to:0}";
-                    publishSensor(mov_topic, end_msg);
-                } else {
-                    String mov_msg = "movimentação ratos: {hour:" + formatter.format(now) + ", from:" + from + ", to:" + to + "}";
-                    publishSensor(mov_topic, mov_msg);
-                    String temp_msg = "temperatura: " + sensorId + ", " + formatter.format(now) + ", " + temperature;
-                    publishSensor(temp_topic, temp_msg);
-                }
+        while (sendingData) {
+            LocalDateTime now = LocalDateTime.now();
+            if (rand.nextDouble() < 0.2) { // randomly end an experience with 20% probability
+                String end_msg = "{hour:" + formatter.format(now) + ", from:0, to:0}";
+                publishSensor(mov_topic, end_msg);
+            } else {
+                String mov_msg = "movimentação ratos: {hour:" + formatter.format(now) + ", from:" + from + ", to:" + to + "}";
+                textArea.append(mov_msg + "\n");
+                publishSensor(mov_topic, mov_msg);
+                String temp_msg = "temperatura: " + sensorId + ", " + formatter.format(now) + ", " + temperature;
+                textArea.append(temp_msg + "\n");
+                publishSensor(temp_topic, temp_msg);
             }
             try {
-                Thread.sleep(1000);
+                synchronized (lock) {
+                    lock.wait(1000);
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
     }
+
+    // Method to notify the thread to resume execution
+    public static void resumeSendingData() {
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+    }
+
 }
